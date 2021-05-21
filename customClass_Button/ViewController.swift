24bea4eb5c90b7c4b8918409e47a1b1a842ae9dd
lee <dog10041@naver.com>
2021-05-21 11:15:42 +0900

    -add init() custom Button

--- customClass_Button/ViewController.swift
+++ customClass_Button/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let customBtn = customButton(frame: CGRect(x: 107, y: 250, width: 200, height: 100))
         
+        let customNewBtn = customButton()
+        
+        customNewBtn.frame = CGRect(x: 107, y: 400, width: 200, height: 100)
+        customNewBtn.backgroundColor = .blue
+        customNewBtn.setTitle("init()버튼", for: .normal)
+        customNewBtn.layer.borderWidth = 3
+        customNewBtn.layer.borderColor = UIColor.red.cgColor
+        
+        self.view.addSubview(customNewBtn)
         self.view.addSubview(customBtn)
     }
 
